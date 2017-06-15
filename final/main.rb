8ddfcf7f475bf109996f7c876bb04c0dc841379e
Marlon Henry Schweigert <fleyhe0@gmail.com>
2017-06-15 12:50:46 -0300

    Initial OpenGL App

---
+++ final/main.rb
@@ -0,0 +1,85 @@
+require 'gl'
+require 'glu'
+require 'glut'
+
+
+include Gl
+include Glu
+include Glut
+
+def onFinishGL
+  glutDestroyWindow $window
+  exit 0
+end
+
+def onRenderEvent
+
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+  glClearColor 1,1,1,0
+
+
+  glutSwapBuffers
+
+end
+
+def onReshapeEvent x, y
+
+  x = 1 if x == 0
+  y = 1 if y == 0
+
+  glViewport 0, 0, x, y
+
+  glMatrixMode GL_PROJECTION
+  glLoadIdentity
+
+  gluPerspective 45, x.to_f/y.to_f, 0.1, 100.0
+
+end
+
+def onKeyboardEvent key, mouseX, mouseY
+
+  case key
+  when ?\e
+    glutDestroyWindow $window
+    exit 0
+  end
+
+  glutPostRedisplay
+end
+
+def onSpecialKeyboardEvent key, mouseX, mouseY
+  glutPostRedisplay
+end
+
+def onMouseButtonEvent button, state, mouseX, mouseY
+  glutPostRedisplay
+end
+
+def onMouseMotionEvent mouseX, mouseY
+  glutPostRedisplay
+end
+
+def onIdleEvent
+  glutPostRedisplay
+end
+
+glutInit
+glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
+glutInitWindowSize 800, 600
+$window = glutCreateWindow "CGREngine 2D"
+
+glutDisplayFunc   method(:onRenderEvent).to_proc
+glutReshapeFunc   method(:onReshapeEvent).to_proc
+glutKeyboardFunc  method(:onKeyboardEvent).to_proc
+glutSpecialFunc   method(:onSpecialKeyboardEvent).to_proc
+glutMouseFunc     method(:onMouseButtonEvent).to_proc
+glutMotionFunc    method(:onMouseMotionEvent).to_proc
+glutIdleFunc      method(:onIdleEvent).to_proc
+
+# glutFullScreen
+
+glEnable GL_TEXTURE_2D
+glEnable GL_DEPTH_TEST
+glEnable GL_CULL_FACE
+
+glutMainLoop()
